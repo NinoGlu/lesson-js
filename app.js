ecd6bda4e1173cf0f129729ad2eb86fb6a6ac255
NinoGlu <glynchadze.nino@gmail.com>
2021-11-04 18:44:46 +0400

    add corret

--- app.js
+++ app.js
@@ -22,28 +22,28 @@ const myImg = document.createElement("img");
 // 3.
 const users = [
   {
-    avatar: "https://i.ytimg.com/vi/VEx5hNYeZtY/hqdefault.jpg",
-    first_name: "John",
-    last_name: "Doe",
-    email: "John.Doe@gmail.com",
+      avatar: "https://i.ytimg.com/vi/VEx5hNYeZtY/hqdefault.jpg",
+      first_name: "John",
+      last_name: "Doe",
+      email: "John.Doe@gmail.com",
   },
   {
-    avatar: "https://i.ytimg.com/vi/VEx5hNYeZtY/hqdefault.jpg",
-    first_name: "John",
-    last_name: "Doe",
-    email: "John.Doe@gmail.com",
+      avatar: "https://i.ytimg.com/vi/VEx5hNYeZtY/hqdefault.jpg",
+      first_name: "John",
+      last_name: "Doe",
+      email: "John.Doe@gmail.com",
   },
   {
-    avatar: "https://i.ytimg.com/vi/VEx5hNYeZtY/hqdefault.jpg",
-    first_name: "John",
-    last_name: "Doe",
-    email: "John.Doe@gmail.com",
+      avatar: "https://i.ytimg.com/vi/VEx5hNYeZtY/hqdefault.jpg",
+      first_name: "John",
+      last_name: "Doe",
+      email: "John.Doe@gmail.com",
   },
   {
-    avatar: "https://i.ytimg.com/vi/VEx5hNYeZtY/hqdefault.jpg",
-    first_name: "John",
-    last_name: "Doe",
-    email: "John.Doe@gmail.com",
+      avatar: "https://i.ytimg.com/vi/VEx5hNYeZtY/hqdefault.jpg",
+      first_name: "John",
+      last_name: "Doe",
+      email: "John.Doe@gmail.com",
   },
 ];
 
@@ -51,20 +51,34 @@ function renderUser() {
   const userTableContainer = document.querySelector("#user-list");
   const userTableBody = userTableContainer.querySelector("tbody");
 
-  document.querySelector("tbody").style.fontStyle = "italic";
-  document.querySelector("tbody").style.color = "#505050";
-
-  const userItems = users.map((user) => {
-    return `<tr> 
-                <td><img src="https://i.ytimg.com/vi/VEx5hNYeZtY/hqdefault.jpg"></td>
-                <td>${user.first_name}</td>
-                <td>${user.last_name}</td>
-                <td><button class="user-remove" type="button">Delete</button></td>
-                <td><button class="user-appear" type="button">Info</button></td>
-            </tr>`;
+  const userItems = users.map(user => {
+      return `<tr> 
+              <td><img alt='${user}' src='${user.avatar}'/></td>
+              <td>${user.first_name}</td>
+              <td>${user.last_name}</td>
+              <td><button class='user_remove' type='button'>remove</button></td>
+              <td><button class='user_info' type='button'>info</button></td>
+          </tr>`;
   });
 
   userTableBody.innerHTML = userItems.join("");
 }
 
 renderUser();
+
+//4.
+const removeBtn = document.querySelectorAll('.user_remove');
+removeBtn.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        const tr = e.target.parentNode.parentNode;
+        tr.remove();
+    })
+})
+
+const addEmail = document.querySelectorAll('.user_info');
+addEmail.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        const tr = e.target.parentNode.parentNode;
+        tr.append(users[0].email);
+    })
+})
